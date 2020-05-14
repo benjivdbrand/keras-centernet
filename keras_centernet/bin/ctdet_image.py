ed837e102fbc00e8a2bcb18ff9aeeda95cefc698
Benji van den Brand <b.p.v.d.brand@student.tue.nl>
2020-05-14 14:39:09 +0200

    Added cutout images with data export file
    Detected cars are now cut out of the image and exported with a separate .txt file containing the following features in order:
    - Box width
    - Box height
    - Box width to height ratio
    - Box area
    - X-coordinate of the box center
    - Y-coordinate of the box center
    - “2D-distance” of the box center from camera
    - “2D-angle” between box center and camera
    - Whether the box is close to the left boundary of the image
    - Whether the box is close to the right boundary of the image

--- keras_centernet/bin/ctdet_image.py
+++ keras_centernet/bin/ctdet_image.py
@@ -3,18 +3,20 @@ import argparse
 import cv2
 import numpy as np
 import os
+import math
 from tqdm import tqdm
 from glob import glob
+import sys
+sys.path.append('/content/keras-centernet')
 
 from keras_centernet.models.networks.hourglass import HourglassNetwork, normalize_image
 from keras_centernet.models.decode import CtDetDecode
 from keras_centernet.utils.utils import COCODrawer
 from keras_centernet.utils.letterbox import LetterboxTransformer
 
-
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--fn', default='assets/demo.jpg', type=str)
+  parser.add_argument('--fn', default='train_images/ID_0a1eb2c76.jpg', type=str)
   parser.add_argument('--output', default='output', type=str)
   parser.add_argument('--inres', default='512,512', type=str)
   args, _ = parser.parse_known_args()
@@ -42,14 +44,51 @@ def main():
     pimg = normalize_image(pimg)
     pimg = np.expand_dims(pimg, 0)
     detections = model.predict(pimg)[0]
+    
+    i = 1
     for d in detections:
+      completeName = os.path.join('output/', os.path.splitext(os.path.basename(fn))[0] + '_' + str(i) + '.txt')         
+
+      if os.path.exists(completeName):
+        os.remove(completeName)
+
+      f = open(completeName,"w+")
+
       x1, y1, x2, y2, score, cl = d
       if score < 0.3:
         break
+      
+      cl = int(cl)
+      name = self.coco_names[cl].split()[-1]
+      if name != 'Car':
+        continue
+       
       x1, y1, x2, y2 = letterbox_transformer.correct_box(x1, y1, x2, y2)
-      img = drawer.draw_box(img, x1, y1, x2, y2, cl)
+      #img = drawer.draw_box(img, x1, y1, x2, y2, cl)
 
-    out_fn = os.path.join(args.output, 'ctdet.' + os.path.basename(fn))
+      croppedIm = img[int(y1):int(y2), int(x1):int(x2)]
+      #out_fn = os.path.join(args.output,  str(i) + '_' + 'ctdet.' + os.path.basename(fn))
+      out_fn = os.path.join(args.output,  os.path.splitext(os.path.basename(fn))[0] + '_' + str(i) + '.jpg')
+      cv2.imwrite(out_fn, croppedIm)
+      
+      f.write(str(x2-x1) + '\n')
+      f.write(str(y2-y1) + '\n')
+      f.write(str((x2-x1)/(y2-y1)) + '\n')
+      f.write(str((x2-x1)*(y2-y1)) + '\n')
+      f.write(str((x2-x1)/2+x1) + '\n')
+      f.write(str((y2-y1)/2+y1) + '\n')
+      f.write(str(math.sqrt(math.pow((y2-y1)/2+y1-len(img[0])/2, 2) + math.pow((x2-x1)/2+x1-len(img), 2))) + '\n')
+      f.write(str(math.atan2((y2-y1)/2+y1-len(img[0])/2, (x2-x1)/2+x1-len(img))) + '\n')
+
+      if((x2-x1)/2+x1 < len(img)):
+        f.write('1\n0')
+      else:
+        f.write('0\n1')
+      
+      i+= 1
+
+    #out_fn = os.path.join(args.output, 'ctdet.' + os.path.basename(fn))
+    out_fn = os.path.join(args.output, os.path.basename(fn))
     cv2.imwrite(out_fn, img)
     print("Image saved to: %s" % out_fn)
 
