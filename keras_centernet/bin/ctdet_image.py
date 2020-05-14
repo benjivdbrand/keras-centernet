88333143b75bb0d832a8cda0e2db6c51c51415ec
Benji van den Brand <b.p.v.d.brand@student.tue.nl>
2020-05-14 21:21:36 +0200

    Connected train dataset to image outputs
    Added world coordinates to pixel coordinates function using the camera matrix. The data of train.csv is connected to the correct image cutouts and is then exported in the same .txt file of that image.

--- keras_centernet/bin/ctdet_image.py
+++ keras_centernet/bin/ctdet_image.py
@@ -2,6 +2,7 @@
 import argparse
 import cv2
 import numpy as np
+import pandas as pd
 import os
 import math
 from tqdm import tqdm
@@ -15,10 +16,14 @@ from keras_centernet.utils.utils import COCODrawer
 from keras_centernet.utils.letterbox import LetterboxTransformer
 
 coco_names = ['person', 'bicycle', 'car', 'motorbike', 'aeroplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'sofa', 'pottedplant', 'bed', 'diningtable', 'toilet', 'tvmonitor', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush',]  # noqa
+camera_matrix = np.array(
+    [[2304.5479, 0,  1686.2379],
+     [0, 2305.8757, 1354.9849],
+     [0, 0, 1]], dtype=np.float32)
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--fn', default='train_images/ID_0a1eb2c76.jpg', type=str)
+  parser.add_argument('--fn', default='train_images/ID_679502721.jpg', type=str)
   parser.add_argument('--output', default='output', type=str)
   parser.add_argument('--inres', default='512,512', type=str)
   args, _ = parser.parse_known_args()
@@ -47,14 +52,23 @@ def main():
     pimg = np.expand_dims(pimg, 0)
     detections = model.predict(pimg)[0]
     
+    train_matrix = train_2_matrix(os.path.splitext(os.path.basename(fn))[0])
+    #train_matrix = train_2_matrix('ID_679502721')
+    x_img = [0] * len(train_matrix[0])
+    y_img = [0] * len(train_matrix[0])
+    for car in range(len(train_matrix[0])):
+      x_img[car] = get_img_coords(train_matrix[4][car], train_matrix[5][car], train_matrix[6][car])[0]
+      y_img[car] = get_img_coords(train_matrix[4][car], train_matrix[5][car], train_matrix[6][car])[1]
+  
+    train_matrix = np.append(train_matrix, [x_img], axis=0)
+    train_matrix = np.append(train_matrix, [y_img], axis=0)
+
     i = 1
     for d in detections:
       completeName = os.path.join('output/', os.path.splitext(os.path.basename(fn))[0] + '_' + str(i) + '.txt')         
 
       if os.path.exists(completeName):
         os.remove(completeName)
-
-      f = open(completeName,"w+")
 
       x1, y1, x2, y2, score, cl = d
       if score < 0.3:
@@ -68,17 +82,33 @@ def main():
       x1, y1, x2, y2 = letterbox_transformer.correct_box(x1, y1, x2, y2)
       #img = drawer.draw_box(img, x1, y1, x2, y2, cl)
 
+      x_center = (x2-x1)/2+x1
+      y_center = (y2-y1)/2+y1
+      car_index = 0
+      faulty = True
+      for car in range(len(train_matrix[0])):
+        if(abs(train_matrix[7][car_index]-x_center) < 10000 / train_matrix[6][car_index] and abs(train_matrix[8][car_index]-y_center) < 10000 / train_matrix[6][car_index]):
+          faulty = False
+          break;  
+
+        car_index += 1
+
+      if(faulty):
+        continue
+
+      f = open(completeName,"w+")
+
       croppedIm = img[int(y1):int(y2), int(x1):int(x2)]
       #out_fn = os.path.join(args.output,  str(i) + '_' + 'ctdet.' + os.path.basename(fn))
       out_fn = os.path.join(args.output,  os.path.splitext(os.path.basename(fn))[0] + '_' + str(i) + '.jpg')
       cv2.imwrite(out_fn, croppedIm)
-      
+
       f.write(str(x2-x1) + '\n')
       f.write(str(y2-y1) + '\n')
       f.write(str((x2-x1)/(y2-y1)) + '\n')
       f.write(str((x2-x1)*(y2-y1)) + '\n')
-      f.write(str((x2-x1)/2+x1) + '\n')
-      f.write(str((y2-y1)/2+y1) + '\n')
+      f.write(str(x_center) + '\n')
+      f.write(str(y_center) + '\n')
       f.write(str(math.sqrt(math.pow((y2-y1)/2+y1-len(img[0])/2, 2) + math.pow((x2-x1)/2+x1-len(img), 2))) + '\n')
       f.write(str(math.atan2((y2-y1)/2+y1-len(img[0])/2, (x2-x1)/2+x1-len(img))) + '\n')
 
@@ -86,6 +116,10 @@ def main():
         f.write('1\n0')
       else:
         f.write('0\n1')
+
+      f.write('\n\n')
+      f.write(str(train_matrix[7][car_index]) + '\n')
+      f.write(str(train_matrix[8][car_index]))
       
       i+= 1
 
@@ -95,5 +129,38 @@ def main():
     print("Image saved to: %s" % out_fn)
 
 
+def get_img_coords(x, y, z):
+    p = np.array([x, y, z]).T
+    img_p = np.dot(camera_matrix, p)
+    img_p[0] /= img_p[2]
+    img_p[1] /= img_p[2]
+    return img_p[0], img_p[1]
+
+
+def train_2_matrix(image_id: str):
+  train = pd.read_csv("train.csv")
+
+  new_lists = [float(item) for item in train[train["ImageId"] == 'ID_679502721']["PredictionString"].tolist()[0].split()]
+
+  matrix_list = [0]
+  for plaats in range(1):
+    matrix_list[plaats] = np.zeros((len(new_lists)//7,7))
+    for i in range(len(new_lists)//7):
+      matrix_list[plaats][i] = new_lists[7*i:7*i+7]
+    matrix_list[plaats] = matrix_list[plaats].transpose()
+  
+  return matrix_list[0]
+
+
 if __name__ == '__main__':
   main()
+  #train_matrix = train_2_matrix('ID_679502721')
+  #x_img = [0] * len(train_matrix[0])
+  #y_img = [0] * len(train_matrix[0])
+  #for car in range(len(train_matrix[0])):
+  #  x_img[car] = get_img_coords(train_matrix[4][car], train_matrix[5][car], train_matrix[6][car])[0]
+  #  y_img[car] = get_img_coords(train_matrix[4][car], train_matrix[5][car], train_matrix[6][car])[1]
+  
+  #train_matrix = np.append(train_matrix, [x_img], axis=0)
+  #train_matrix = np.append(train_matrix, [y_img], axis=0)
+  #print(train_matrix)
